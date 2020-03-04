b5054196fdfe2aefce96591325a85de528530e27
codeaprendiz <codeaprendiz@gmail.com>
2020-03-05 01:23:54 +0400

    Chore: update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # terraform-kitchen
+
+
+```bash
+terraform init
+
+# To apply and execute the change
+terraform apply
+
+# To destroy the infra
+terraform destroy
+
+# Testing what infrastructure would be built based on our terraform files
+terraform plan
+
+# Save the changes in out file
+terraform plan -out out.terraform
+
+# Only apply the changes saved in outfile
+terraform apply out.terraform
+```
--- task1/instance.tf
+++ task1/instance.tf
@@ -1,12 +1,41 @@
+//provider "aws" {
+//  access_key = ""
+//  secret_key = ""
+//  region     = "eu-west-1"
+//}
+//
+////https://cloud-images.ubuntu.com/locator/ec2/
+//resource "aws_instance" "example" {
+//  ami           = "ami-035df3cfe31bdea3b"
+//  instance_type = "t2.micro"
+//}
+
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region = "eu-west-1"
 }
 
-//https://cloud-images.ubuntu.com/locator/ec2/
-resource "aws_instance" "example" {
-  ami           = "ami-035df3cfe31bdea3b"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-}
 
+  tags = {
+    Name = "HelloWorld"
+  }
+}
